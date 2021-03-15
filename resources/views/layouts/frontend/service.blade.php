39546bda42eafb8fce06ca9053e0f51442dd8091
Hasinur Rahman <hasinur202@gmail.com>
2021-03-15 12:13:42 +0600

    services page design decorate

--- resources/views/layouts/frontend/service.blade.php
+++ resources/views/layouts/frontend/service.blade.php
@@ -2,7 +2,13 @@
 
 @section('css')
 <style>
-
+    .service-title{
+        display: contents;
+        width: 100%;
+    }
+    .tab-content-align p{
+        text-align: justify;
+    }
 </style>
 @endsection
 
@@ -33,19 +39,19 @@
 
 <section>
     <div class="container" style="margin-top: 2rem">
-        <div class="row service">
-            <div class="title-left">
-                <h2>Our Service</h2>
+
+        <div class="row service" style="margin-top: 2rem">
+            <div class="service-title">
+                <h3>Website Development</h3>
             </div>
             <div class="col-md-12">
                 <!-- Custom Tabs -->
                 <div class="nav-tabs-custom">
                     <div class="tab-content row">
                         <div class="col-md-12">
-
-                            <div class="col-md-6 tab-content-part">
-                                <h2>Testing</h2>
-                                <p>Exactly like the original bootstrap tabs except you should use the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
+                            <div class="col-md-6 tab-content-align">
+                                <p>Exactly like the original bootstrap tabs except you should use_ the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
+                                    I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend Exactly like the original bootstrap tabs except you should use_ the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
                                     I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend</p>
                             </div>
                             <div class="col-md-6">
@@ -61,24 +67,23 @@
                 <!-- nav-tabs-custom -->
             </div>
         </div>
-        <div class="row service">
-            <div class="title-right">
-                <h2>Mission & Vision</h2>
+
+        <div class="row service" style="margin-top: 2rem">
+            <div class="service-title">
+                <h3 style="text-align: right">Software Development</h3>
             </div>
             <div class="col-md-12">
                 <!-- Custom Tabs -->
                 <div class="nav-tabs-custom">
                     <div class="tab-content row">
                         <div class="col-md-12">
-
                             <div class="col-md-6">
                                 <figure>
                                     <img class="tab-figure" src="/images/Customized-Software-Development.gif " alt=" ">
                                 </figure>
                             </div>
-                            <div class="col-md-6 tab-content-part">
-                                <h2>Testing</h2>
-                                <p>Exactly like the original bootstrap tabs except you should use the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
+                            <div class="col-md-6 tab-content-align">
+                                <p>Exactly like the original bootstrap tabs except you should _use the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
                                     I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend</p>
                             </div>
                         </div>
@@ -89,6 +94,118 @@
                 <!-- nav-tabs-custom -->
             </div>
         </div>
+        <div class="row service" style="margin-top: 2rem">
+            <div class="service-title">
+                <h3>Graphics Design</h3>
+            </div>
+            <div class="col-md-12">
+                <!-- Custom Tabs -->
+                <div class="nav-tabs-custom">
+                    <div class="tab-content row">
+                        <div class="col-md-12">
+                            <div class="col-md-6 tab-content-align">
+                                <p>Exactly like the original bootstrap tabs except you should use_ the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
+                                    I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend</p>
+                            </div>
+                            <div class="col-md-6">
+                                <figure>
+                                    <img class="tab-figure" src="/images/Customized-Software-Development.gif" alt=" ">
+                                </figure>
+                            </div>
+                        </div>
+                        <!-- /.tab-pane -->
+                    </div>
+                    <!-- /.tab-content -->
+                </div>
+                <!-- nav-tabs-custom -->
+            </div>
+        </div>
+
+        <div class="row service" style="margin-top: 2rem">
+            <div class="service-title">
+                <h3 style="text-align: right">Digital Marketing</h3>
+            </div>
+            <div class="col-md-12">
+                <!-- Custom Tabs -->
+                <div class="nav-tabs-custom">
+                    <div class="tab-content row">
+                        <div class="col-md-12">
+                            <div class="col-md-6">
+                                <figure>
+                                    <img class="tab-figure" src="/images/Customized-Software-Development.gif " alt=" ">
+                                </figure>
+                            </div>
+                            <div class="col-md-6 tab-content-align">
+                                <p>Exactly like the original bootstrap tabs except you should _use the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
+                                    I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend</p>
+                            </div>
+                        </div>
+                        <!-- /.tab-pane -->
+                    </div>
+                    <!-- /.tab-content -->
+                </div>
+                <!-- nav-tabs-custom -->
+            </div>
+        </div>
+        <div class="row service" style="margin-top: 2rem">
+            <div class="service-title">
+                <h3>E-Commerce</h3>
+            </div>
+            <div class="col-md-12">
+                <!-- Custom Tabs -->
+                <div class="nav-tabs-custom">
+                    <div class="tab-content row">
+                        <div class="col-md-12">
+                            <div class="col-md-6 tab-content-align">
+                                <p>Exactly like the original bootstrap tabs except you should use_ the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
+                                    I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend</p>
+                            </div>
+                            <div class="col-md-6">
+                                <figure>
+                                    <img class="tab-figure" src="/images/Customized-Software-Development.gif" alt=" ">
+                                </figure>
+                            </div>
+                        </div>
+                        <!-- /.tab-pane -->
+                    </div>
+                    <!-- /.tab-content -->
+                </div>
+                <!-- nav-tabs-custom -->
+            </div>
+        </div>
+
+        <div class="row service" style="margin-top: 2rem">
+            <div class="service-title pull-right">
+                <h3 style="text-align: right">Hosting & Domain</h3>
+            </div>
+            <div class="col-md-12">
+                <!-- Custom Tabs -->
+                <div class="nav-tabs-custom">
+                    <div class="tab-content row">
+                        <div class="col-md-12">
+                            <div class="col-md-6">
+                                <figure>
+                                    <img class="tab-figure" src="/images/Customized-Software-Development.gif " alt=" ">
+                                </figure>
+                            </div>
+                            <div class="col-md-6 tab-content-align">
+                                <p>Exactly like the original bootstrap tabs except you should _use the custom wrapper. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.
+                                    I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend</p>
+                            </div>
+                        </div>
+                        <!-- /.tab-pane -->
+                    </div>
+                    <!-- /.tab-content -->
+                </div>
+                <!-- nav-tabs-custom -->
+            </div>
+        </div>
+
+
+
+
+
+
     </div>
 </section>
 
