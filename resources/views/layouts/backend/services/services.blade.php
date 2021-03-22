ebdd7ec9a81592e3531dc93a8a18bcd17ba71e2a
Hasinur Rahman <hasinur202@gmail.com>
2021-03-22 12:28:53 +0600

    data show in edit form

--- resources/views/layouts/backend/services/services.blade.php
+++ resources/views/layouts/backend/services/services.blade.php
@@ -98,6 +98,67 @@
                 </div>
             </div>
 
+            <div class="row" id="editService" style="display: none">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header bg-warning">
+                            <h5 class="float-left text-white">Edit Services</h5>
+                            <a href="javascript:void(0)" class="float-right" onclick="closeAddNew()"><i class="fa fa-times"></i></a>
+                        </div>
+                        <form method="POST" action="{{ route('services.store') }}" enctype="multipart/form-data">
+                            @csrf
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-5 float-left">
+                                        <div class="form-group">
+                                            <input name="title" id="title" placeholder="Title" type="text" required class="form-control">
+                                            <input name="id" id="id" type="hidden" class="form-control">
+                                        </div>
+
+                                        <div class="form-group float-left" style="width: 40%">
+                                            <label>Icon </label>
+                                            <div class="service-img">
+                                                <input id="edit_icon" type="file" class="form-control" name="icon">
+                                                <img src="" id="edit_icon_img"/>
+                                            </div>
+                                        </div>
+                                        <div class="form-group float-right" style="width: 56%">
+                                            <label>Image </label>
+                                            <div class="service-img" style="width: 70% !important">
+                                                <input id="edit_image" type="file" class="form-control" name="image">
+                                                <img src="" id="edit_image_img"/>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-7 float-right">
+                                        <div class="form-group">
+                                            <textarea name="description" id="description" placeholder="Description" required class="form-control" style="min-height: 108px !important"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <textarea name="short_description" id="short_description" placeholder="Short Description" required class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-12">
+                                    <label style="width: 100%">SEO</label>
+                                    <div class="form-group">
+                                        <input name="icon_alt" id="icon_alt" placeholder="Icon Alt" maxlength="100" size="100" type="text" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <input name="image_alt" id="image_alt" placeholder="Image Alt" maxlength="100" size="100" type="text" class="form-control">
+                                    </div>
+                                    <button type="submit" class="btn btn-success"></i> Save Changes</button>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
 
             <div class="row" id="serviceList">
                 <div class="col-md-12">
@@ -139,8 +200,8 @@
                                             <td>{{ Str::limit($service->short_description,80,'...') }}</td>
 
                                             <td style="display:inline-flex;">
+                                                <button onclick="editService({{ $service }})" style="margin-right: 5px" class="btn btn-dark btn-xs"><i class="fa fa-edit"></i></button>
                                                 <button class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></button>
-                                                <button style="margin-left: 5px" class="btn btn-dark btn-xs"><i class="fa fa-edit"></i></button>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -182,6 +243,23 @@
     function closeAddNew(){
         $("#newService").hide();
         $("#serviceList").show();
+        $("#editService").hide();
+    }
+
+    function editService(service){
+        $("#newService").hide();
+        $("#serviceList").hide();
+        $("#editService").show();
+
+        $("#id").val(service.id);
+        $("#title").val(service.title);
+        $("#edit_icon_img").attr('src', "{{ asset('/images/services/') }}/" + service.icon);
+        $("#edit_image_img").attr('src', "{{ asset('/images/services/') }}/" + service.image);
+
+        $("#description").val(service.description);
+        $("#short_description").val(service.short_description);
+        $("#icon_alt").val(service.icon_alt);
+        $("#image_alt").val(service.image_alt);
     }
 
 
@@ -211,6 +289,35 @@
     $("#image").change(function() {
         imageUrl(this);
     });
+
+    function editiconUrl(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#edit_icon_img').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    function editimageUrl(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#edit_image_img').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#edit_icon").change(function() {
+        editiconUrl(this);
+    });
+    $("#edit_image").change(function() {
+        editimageUrl(this);
+    });
+
+
 </script>
 
 @endsection
