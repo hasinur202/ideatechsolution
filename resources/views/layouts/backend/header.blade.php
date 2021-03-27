e086baf9bcb9b7916f627d0f9ab301e489623e66
Hasinur Rahman <hasinur202@gmail.com>
2021-03-27 18:23:23 +0600

    add google verification script

--- resources/views/layouts/backend/header.blade.php
+++ resources/views/layouts/backend/header.blade.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <script src="https://www.googleoptimize.com/optimize.js?id=OPT-TFXZJ44"></script>
 <head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5PMDXM6');
+    </script>
+    <!-- End Google Tag Manager -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -46,6 +56,10 @@
     </style>
 </head>
 <body class="hold-transition sidebar-mini">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PMDXM6"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
 <div class="wrapper">
 
